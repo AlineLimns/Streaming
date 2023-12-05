7169f8f479a3ea9ac198a6891950700ee2380fca
AlineLimns <37154951+AlineLimns@users.noreply.github.com>
2023-12-04 21:11:02 -0300

    Atualização
    Firebase tualizado

--- js/app.js
+++ js/app.js
@@ -1,32 +1,4 @@
-function showSignup() {
-    document.getElementById('loginContainer').style.display = 'none';
-    document.getElementById('signupContainer').style.display = 'block';
-}
-
-function showLogin() {
-    document.getElementById('signupContainer').style.display = 'none';
-    document.getElementById('loginContainer').style.display = 'block';
-}
-
-function login() {
-    const email = document.getElementById('email').value;
-    const password = document.getElementById('password').value;
-
-    firebase.auth().signInWithEmailAndPassword(email, password)
-        .then((userCredential) => {
-            // Usuário autenticado com sucesso
-            console.log('Usuário autenticado:', userCredential.user.email);
-             // Redirecionar para a página home.html
-            window.location.href = 'home.html'
-        })
-       
-
-        .catch((error) => {
-            alert(error.message);
-        });
-
-}
-
+// Função para cadastrar usuário
 function signup() {
     const email = document.getElementById('signupEmail').value;
     const password = document.getElementById('signupPassword').value;
@@ -46,3 +18,33 @@ function signup() {
             alert(error.message);
         });
 }
+
+// Função para mostrar o formulário de login
+function showLogin() {
+    document.getElementById('signupContainer').style.display = 'none';
+    document.getElementById('loginContainer').style.display = 'block';
+}
+
+// Função para ocultar o formulário de login e mostrar o formulário de cadastro
+function showSignup() {
+    document.getElementById('loginContainer').style.display = 'none';
+    document.getElementById('signupContainer').style.display = 'block';
+}
+
+// Função para efetuar o login
+function login() {
+    const email = document.getElementById('email').value;
+    const password = document.getElementById('password').value;
+
+    firebase.auth().signInWithEmailAndPassword(email, password)
+        .then((userCredential) => {
+            // Usuário autenticado com sucesso
+            console.log('Usuário autenticado:', userCredential.user.email);
+            // Redirecionar para a página home.html
+            window.location.href = 'home.html';
+        })
+        .catch((error) => {
+            alert(error.message);
+        });
+}
+
--- js/firebase-config.js
+++ js/firebase-config.js
@@ -1,10 +1,12 @@
+
 const firebaseConfig = {
-    apiKey: "AIzaSyAHGjhlPy4-2vuKqihtnAmBKoaHIzdgnC4",
-    authDomain: "streaming-3d135.firebaseapp.com",
-    projectId: "streaming-3d135",
-    storageBucket: "streaming-3d135.appspot.com",
-    messagingSenderId: "901436912354",
-    appId: "1:901436912354:web:15854064e9ef746660a4bb"
-  };
-  // Initialize Firebase
-  firebase.initializeApp(firebaseConfig);
+  apiKey: "AIzaSyAHGjhlPy4-2vuKqihtnAmBKoaHIzdgnC4",
+  authDomain: "streaming-3d135.firebaseapp.com",
+  projectId: "streaming-3d135",
+  storageBucket: "streaming-3d135.appspot.com",
+  messagingSenderId: "901436912354",
+  appId: "1:901436912354:web:15854064e9ef746660a4bb"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
